5fe840aef8e725ac5a170f5d056b0ba1a1038097
Fungki Pandu F <neopandu@gmail.com>
2019-10-06 17:52:28 +0700

    change time from dev mode to real

--- app/src/main/java/xyz/neopandu/moov/data/repository/ReminderRepository.kt
+++ app/src/main/java/xyz/neopandu/moov/data/repository/ReminderRepository.kt
@@ -13,21 +13,21 @@ class ReminderRepository(private val context: Context) {
     fun setReminder(type: AlarmReceiver.ReminderType) {
 
         // easy for maintenance
-//        val timeArray = when (type) {
-//            AlarmReceiver.ReminderType.DAILY_REMINDER -> intArrayOf(7, 0)
-//            AlarmReceiver.ReminderType.RELEASE_REMINDER -> intArrayOf(8, 0)
-//            AlarmReceiver.ReminderType.AIRING_TODAY_REMINDER -> intArrayOf(7, 30)
-//        }
+        val timeArray = when (type) {
+            AlarmReceiver.ReminderType.DAILY_REMINDER -> intArrayOf(7, 0)
+            AlarmReceiver.ReminderType.RELEASE_REMINDER -> intArrayOf(8, 0)
+            AlarmReceiver.ReminderType.AIRING_TODAY_REMINDER -> intArrayOf(7, 30)
+        }
 
         val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
         val intent = Intent(context, AlarmReceiver::class.java)
         intent.putExtra(AlarmReceiver.EXTRA_TYPE, type.id)
 
         val calendar = Calendar.getInstance()
-//        calendar.set(Calendar.HOUR_OF_DAY, Integer.parseInt(timeArray[0]))
-//        calendar.set(Calendar.MINUTE, Integer.parseInt(timeArray[1]))
-//        calendar.set(Calendar.SECOND, 0)
-        calendar.add(Calendar.SECOND, 30) // for development only
+        calendar.set(Calendar.HOUR_OF_DAY, timeArray[0])
+        calendar.set(Calendar.MINUTE, timeArray[1])
+        calendar.set(Calendar.SECOND, 0)
+//        calendar.add(Calendar.SECOND, 30) // for development only
 
         val pendingIntent = PendingIntent.getBroadcast(context, type.id, intent, 0)
         alarmManager.setInexactRepeating(
